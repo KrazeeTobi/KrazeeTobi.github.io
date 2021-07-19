4c515466489f6c2e70602dcd717aee8379cf4d33
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2021-07-19 16:00:37 +0100

    More files removed in AGB (forgot about the rest)

--- sbspss/agb/spongebobsquarepants/spongebobsquarepants/Source/Clean.bat
+++
@@ -1,7 +0,0 @@
-cls
-@echo off
-rem *** clean up ***
-if exist *.o del *.o
-if exist *.elf del *.elf
-if exist *.bin del *.bin
-if exist *.map del *.map
--- sbspss/agb/spongebobsquarepants/spongebobsquarepants/Source/mc.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-make clean
-make clean_data
-call clean.bat
-make data
-make depend
-make
