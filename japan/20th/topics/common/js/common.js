78943a23af55cd44f7ae6cd3e76b23c5f805ace8
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2021-12-28 14:25:45 +0000

    Remove quite a few files we no longer need

--- japan/20th/topics/common/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	var menu = $('.bodyTopics #contents-navigation').smoothMove();
-	
-	var _agent = navigator.userAgent;
-	if( _agent.indexOf('iPhone') != -1 || _agent.indexOf('iPod') != -1 || _agent.indexOf('iPad') != -1) {
-		$('body').css('background','#fff url(../common/css/common-tag/iphone-body.gif) no-repeat center 60px scroll');
-	
-	}
-});
