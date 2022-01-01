1f2fbc2a991f0d11bbd0ba7d56082198ed026640
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-01-01 22:34:05 +0000

    more SonicChannelOld stuff

---
+++ japan/SonicChannelOld/enjoy/blogparts/js/sonic01.js
@@ -0,0 +1,8 @@
+document.write(
+		'<object data="http://sonic.sega.jp/enjoy/blogparts/swf/index.swf" width="145" height="230" type="application/x-shockwave-flash" id="psupita" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0">'+
+		'<param name="movie" value="http://sonic.sega.jp/enjoy/blogparts/swf/index.swf" />'+
+		'<param name="quality" value="high" />'+
+		'<param name="menu" value="false">'+
+		'<embed src="http://sonic.sega.jp/enjoy/blogparts/swf/index.swf" quality="high" menu="false" width="145" height="230" name="psupita" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'+
+		'</object>'
+	);
